d8b19fd50a3c8b8970bc0292b47f2427ce3c96a1
NVital14 <96735261+NVital14@users.noreply.github.com>
2023-06-01 13:45:31 +0100

    commit3

---
+++ src/tetris/lib/board/TetrisGame.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tetris.lib.board;
+
+import java.util.Timer;
+
+/**
+ *
+ * @author vital
+ */
+public class TetrisGame extends board{
+    /**
+     * Linhas completas de blocos e tempo
+     */
+    private int completedLines;
+    private Timer time;
+    
+    public TetrisGame(int lines, int columns){
+        
+    }
+    
+}
